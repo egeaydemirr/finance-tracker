29d64fff7f3f20e24832b3a13ffefc47bd9934d7
Ege Aydemir <egeaydemirr97@gmail.com>
2023-05-13 23:40:24 +0300

    logo and some style changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 
 function App() {
   const [description, setDescription] = useState("");
   const [amount, setAmount] = useState();
   const [finalAmount, setFinalAmount] = useState(0);
   const [transaction, setTransaction] = useState([]);
-  const [baseCurrency, setBaseCurrency] = useState("usd");
-  const [currencyType, setCurrencyType] = useState("usd");
+  const [baseCurrency, setBaseCurrency] = useState("USD");
+  const [currencyType, setCurrencyType] = useState("USD");
   const [currencies, setCurrencies] = useState([]);
   const [exchangeRate, setExchangeRate] = useState(1);
   const [currentExchangeRate, setCurrentExchangeRate] = useState(1);
@@ -21,6 +21,7 @@ function App() {
     setAmount(item.amount);
     setCurrencyType(item.currencyType);
     setShowModal(true);
+    
   };
 
   //handleIncome gelir ekleme fonksiyonudur. Kullanicinin edit butonuna basma ihtimalina karsn bir if else olusturdum.
@@ -118,13 +119,15 @@ function App() {
     <div className="bg-gradient-to-r from-blue-300 to-indigo-500 min-h-screen">
       <h1 className="text-3xl font-bold text-center py-6">Finance Tracker</h1>
       <div className="container mx-auto mt-20">
+      <img src="public/logo.png" alt="Logo" className="w-32 absolute top-10 left-10" />
+
         <div className="p-5  rounded-lg">
           <h2 className="text-lg font-semibold mb-4 ">
             Total Amount: {(finalAmount * exchangeRate).toFixed(1)}{" "}
             {baseCurrency.toUpperCase()}
           </h2>
-          <div className="mb-4">
-            <label htmlFor="currency" className="mr-2">
+          <div className="mb-4  flex flex-row">
+            <label htmlFor="currency" className="mr-2 rounded-md w ">
               Change Base Currency
             </label>
             <select
@@ -142,57 +145,58 @@ function App() {
           </div>
           <h2 className="text-2xl font-bold mb-4">Transactions</h2>
 
-          <table className="w-full table-fixed text-center">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="px-4 py-2 text-center text-gray-700 font-bold">
-                  DESCRIPTION
-                </th>
-                <th className="px-4 py-2 text-center text-gray-700 font-bold">
-                  AMOUNT
-                </th>
-                <th className="px-4 py-2 text-center text-gray-700 font-bold">
-                  CURRENCY
-                </th>
-                <th className="px-4 py-2 text-center text-gray-700 font-bold">
-                  DATE
-                </th>
-                <th className="px-4 py-2 text-center text-gray-700 font-bold">
-                  ACTION
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {transaction.map(item => (
-                <tr key={item.id} className={item.color}>
-                  <td className="px-4 py-2  text-gray-700 text-center">
-                    {item.description}
-                  </td>
-                  <td className="px-4 py-2  text-gray-700 text-center">
-                    {item.amount}
-                  </td>
-                  <td className="px-4 py-2  text-gray-700 text-center">
-                    {item.currencyType.toUpperCase()}
-                  </td>
-                  <td className="px-4 py-2  text-gray-700 text-center">
-                    <div className="text-sm text-gray-500">{item.date}</div>
-                    <div className="text-xs text-gray-500">{item.time}</div>
-                  </td>
-                  <td className="px-4 py-2 text-left">
-                    <button
-                      className="w-20 bg-gradient-to-r from-green-500 to-green-900 hover:from-green-700 hover:to-teal-500 text-white p-2 mx-2 rounded-md shadow-lg duration-300 hover:-translate-y-1"
-                      onClick={e => handleEdit(item)}
-                    >
-                      Edit
-                    </button>
-                    <button className="w-20 bg-gradient-to-r from-red-500 to-red-900 hover:from-red-700 hover:to-rose-500 text-white p-2 mx-2 rounded-md shadow-lg duration-300 hover:-translate-y-1">
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+          <table className="w-full table-fixed text-center rounded-lg overflow-hidden">
+  <thead>
+    <tr className="bg-gray-200">
+      <th className="px-4 py-2 text-center text-gray-700 font-bold">
+        DESCRIPTION
+      </th>
+      <th className="px-4 py-2 text-center text-gray-700 font-bold">
+        AMOUNT
+      </th>
+      <th className="px-4 py-2 text-center text-gray-700 font-bold">
+        CURRENCY
+      </th>
+      <th className="px-4 py-2 text-center text-gray-700 font-bold">
+        DATE / TIME
+      </th>
+      <th className="px-4 py-2 text-center text-gray-700 font-bold">
+        ACTION
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    {transaction.map(item => (
+      <tr key={item.id} className={item.color}>
+        <td className="px-4 py-2  text-gray-700 text-center">
+          {item.description}
+        </td>
+        <td className="px-4 py-2  text-gray-700 text-center">
+          {item.amount}
+        </td>
+        <td className="px-4 py-2  text-gray-700 text-center">
+          {item.currencyType.toUpperCase()}
+        </td>
+        <td className="px-4 py-2  text-gray-700 text-center">
+          <div className="text-sm text-gray-500">{item.date}</div>
+          <div className="text-xs text-gray-500">{item.time}</div>
+        </td>
+        <td className="px-4 py-2 text-left">
+          <button
+            className="w-20 bg-gradient-to-r from-green-500 to-green-900 hover:from-green-700 hover:to-teal-500 text-white p-2 mx-2 rounded-md shadow-lg duration-300 hover:-translate-y-1"
+            onClick={() => handleEdit(item)}
+          >
+            Edit
+          </button>
+          <button className="w-20 bg-gradient-to-r from-red-500 to-red-900 hover:from-red-700 hover:to-rose-500 text-white p-2 mx-2 rounded-md shadow-lg duration-300 hover:-translate-y-1">
+            Delete
+          </button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</table>
+
 
           <div className="flex justify-center pt-16">
             <button
