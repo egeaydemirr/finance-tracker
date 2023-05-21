e2ee69718afae5ca2c985d5709fbbd57a8ad06cb
Ege Aydemir <egeaydemirr97@gmail.com>
2023-05-21 14:10:07 +0300

    loading spinner style

--- src/App.jsx
+++ src/App.jsx
@@ -310,12 +310,14 @@ function App() {
         <div className="p-5  rounded-lg">
           <div className="flex flex-row justify-between">
             <h2 className=" text-lg font-semibold mb-4 ">
+            <div className="flex flex-row">
               Total Amount:{" "}
+              
               {loading ? (
-                <div role="status">
+                <div role="status" >
                   <svg
                     aria-hidden="true"
-                    className="w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+                    className="w-6 h-8 ml-2  text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
                     viewBox="0 0 100 101"
                     fill="none"
                     xmlns="http://www.w3.org/2000/svg"
@@ -337,6 +339,7 @@ function App() {
                   currency: baseCurrency,
                 })
               )}
+              </div>
             </h2>
           </div>
           <div className="mb-4  flex flex-row">
